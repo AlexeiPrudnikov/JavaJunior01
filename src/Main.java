79fce2c3740201fefc3657ae29181cde93129e2f
Alexei <alexei.prudnikov@gmail.com>
2023-11-25 13:49:45 +0300

    add part 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
+    public static final int SIZE = 10;
+    public static final int MAX_VALUE = 1_000;
+    public static final int MIN_VALUE = 1;
+
     public static void main(String[] args) {
         /**
          * 0.1. Посмотреть разные статьи на Хабр.ру про Stream API
@@ -16,6 +24,24 @@ public class Main {
          * 2.4 * Из списка сотрудников с помощью стрима создать Map<String, List<Employee>> с отделами и сотрудниками внутри отдела
          * 2.5 * Из списока сорудников с помощью стрима создать Map<String, Double> с отделами и средней зарплатой внутри отдела
          */
-        
+        Random random = new Random();
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < SIZE; i++) {
+            numbers.add(random.nextInt(MAX_VALUE - MIN_VALUE + 1) + MIN_VALUE);
+        }
+        System.out.println(numbers);
+        System.out.println("==========");
+        System.out.printf("Максимальное число: %d\n", numbers.stream().max(Integer::compareTo).get());
+        System.out.println("==========");
+        System.out.println("Все числа, большие, чем 500_000, умножить на 5, отнять от них 150 и просуммировать");
+        System.out.println(numbers.stream()
+                .filter(x -> x > 500_000)
+                .mapToLong(x -> x.intValue() * 5 - 150)
+                .sum());
+        System.out.println("==========");
+        System.out.println("Найти количество чисел, квадрат которых меньше, чем 100_000");
+        System.out.println(numbers.stream()
+                .filter(x -> Math.pow(x, 2) < 100_000)
+                .count());
     }
 }
